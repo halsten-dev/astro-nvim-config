668acf924bf4a7991cc934c38d2b72e0f5b0e1f6
Halsten <leeser.lionel@gmail.com>
2025-10-02 09:10:34 +0000

    Added mapping for the half page move
    ctrl+d and ctrl+u

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,16 @@
+-- Remap Ctrl+d / u in normal mode to center the view.
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          ["<C-d>"] = { "<C-d>zz", desc = "Scroll down and center" },
+          ["<C-u>"] = { "<C-u>zz", desc = "Scroll up and center" },
+        },
+      },
+    },
+  },
+}
