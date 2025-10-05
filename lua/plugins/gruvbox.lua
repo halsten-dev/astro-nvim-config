4e75c1c7e869c126be0fde87c7531f36167046e1
Halsten <leeser.lionel@gmail.com>
2025-10-05 16:03:35 +0200

    Added and used gruvbox theme

---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "ellisonleao/gruvbox.nvim",
+    name = "gruvbox",
+    opts = {
+      italic = {
+        strings = true,
+        comments = true,
+        folds = true,
+        operations = false,
+      }
+    }
+  }
+}
