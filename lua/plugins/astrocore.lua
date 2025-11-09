38de7780b4cb55e741087184957c6b2ad505ba91
Halsten <leeser.lionel@gmail.com>
2025-11-09 10:32:01 +0100

    Small adjustements, fixed the tab issue

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -12,12 +12,12 @@ return {
   opts = {
     -- Configure core features of AstroNvim
     features = {
-      large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
-      autopairs = true, -- enable autopairs at start
-      cmp = true, -- enable completion at start
+      large_buf = { size = 1024 * 256, lines = 10000 },             -- set global limits for large files for disabling features like treesitter
+      autopairs = true,                                             -- enable autopairs at start
+      cmp = true,                                                   -- enable completion at start
       diagnostics = { virtual_text = true, virtual_lines = false }, -- diagnostic settings on startup
-      highlighturl = true, -- highlight URLs at start
-      notifications = true, -- enable notifications at start
+      highlighturl = true,                                          -- highlight URLs at start
+      notifications = true,                                         -- enable notifications at start
     },
     -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
     diagnostics = {
@@ -39,12 +39,12 @@ return {
     },
     -- vim options can be configured here
     options = {
-      opt = { -- vim.opt.<key>
+      opt = {                  -- vim.opt.<key>
         relativenumber = true, -- sets vim.opt.relativenumber
-        number = true, -- sets vim.opt.number
-        spell = false, -- sets vim.opt.spell
-        signcolumn = "yes", -- sets vim.opt.signcolumn to yes
-        wrap = false, -- sets vim.opt.wrap
+        number = true,         -- sets vim.opt.number
+        spell = false,         -- sets vim.opt.spell
+        signcolumn = "yes",    -- sets vim.opt.signcolumn to yes
+        wrap = false,          -- sets vim.opt.wrap
         tabstop = 4,
         shiftwidth = 4,
         softtabstop = 4,
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,6 +1,33 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+--if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This will run last in the setup process.
 -- This is just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
+--
 
+return {
+    plugins = {
+        cmp = function(config)
+            local cmp = require "cmp"
+            local luasnip = require "luasnip"
+            config.mapping["<Tab>"] = cmp.mapping(function(fallback)
+                if luasnip.expandable() then
+                    luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end, { "i", "s" })
+            config.mapping["<S-Tab>"] = cmp.mapping(function(fallback)
+                if luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, { "i", "s" })
+
+            return config
+        end,
+    },
+}
